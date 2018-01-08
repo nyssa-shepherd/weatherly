6fe23d6af087175f1a9bd6e31fba0ef2440d01c9
Nyssa Keller <nyssa1@gmail.com>
2018-01-08 10:41:35 -0700

    Fix tenday and sevenhour tests

--- test/TenDay.test.js
+++ test/TenDay.test.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import TenDay from '../lib/TenDay.js';
+import MockData from '../lib/MockData';
 
 describe('TenDay', () => {
   
   let wrapper; 
 
   beforeEach(() => {
-    wrapper = shallow(<TenDay />)
+    wrapper = shallow(<TenDay forecast={MockData.forecast.simpleforecast.forecastday.slice(0, 10)} />)
   })
   
-  it.skip('should exist', () => {
-    let wrapper = shallow(<TenDay />);
+  it('should exist', () => {
     expect(wrapper).toBeDefined();
+  })
+
+  it('should render 10 Card components', () => {
+    expect(wrapper.find('Card').length).toEqual(10);
+  })
+
+  it('should render an h2 components', () => {
+    expect(wrapper.find('h2').length).toEqual(1);
   })
 
 })
--- test/Welcome.test.js
+++ test/Welcome.test.js
@@ -14,4 +14,21 @@ describe('Welcome', () => {
     expect(wrapper).toBeDefined();
   })
 
+  it('should have a default state of city, stateName, and location equal to an empty string', () => {
+    expect(wrapper.state().city).toEqual('');
+    expect(wrapper.state().stateName).toEqual('');
+    expect(wrapper.state().location).toEqual('');
+  })
+
+  it('should have a default state suggestedWord equal to an empty array', () => {
+    expect(wrapper.state().suggestedWord).toEqual([]);
+  })
+
+  it('should render an article, h1, input, and button component', () => {
+    expect(wrapper.find('article').length).toEqual(1);
+    expect(wrapper.find('h1').length).toEqual(1);
+    expect(wrapper.find('input').length).toEqual(1);
+    expect(wrapper.find('button').length).toEqual(1);
+  })
+
 })
